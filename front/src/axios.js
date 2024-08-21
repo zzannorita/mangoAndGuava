524c10cb13fc3ec2f8ecdda36a123bfc44644c2f
zzannorita <tpals0518@naver.com>
2024-08-21 18:20:00 +0900

    Feat: axiosInstance로 파일 구조화

--- front/src/axios.js
+++ front/src/axios.js
@@ -1,5 +1,34 @@
-import React from "react";
+// src/api.js
+import axios from "axios";
+import Cookies from "js-cookie";
 
-export default function axios() {
-  return <div></div>;
-}
+const axiosInstance = axios.create({
+  baseURL: "http://localhost:3001", // 기본 API URL 설정
+});
+
+// 요청 인터셉터
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("accessToken");
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+// 응답 인터셉터
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      console.error("응답 에러");
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
