5f356a1e0549109550c91ee31594a699a0aa4805
zzannorita <tpals0518@naver.com>
2024-12-01 00:36:09 +0900

    Fix: 엔터 키 입력 시 검색 실행

--- front/src/components/SearchBox.js
+++ front/src/components/SearchBox.js
@@ -5,6 +5,11 @@ import search from "../image/search.png";
 const SearchBox = () => {
   const { query, onChange, handleSearch } = useSearch();
 
+  const handleEnter = (e) => {
+    if (e.key === "Enter") {
+      handleSearch(); // 엔터 키 입력 시 검색 실행
+    }
+  };
   return (
     <div className={SearchBoxStyle.searchContainer}>
       <input
@@ -12,6 +17,7 @@ const SearchBox = () => {
         className="searchBox"
         value={query}
         onChange={onChange}
+        onKeyDown={handleEnter}
         placeholder="찾으시는 상품을 검색하세요."
       />
       <button className={SearchBoxStyle.searchButton} onClick={handleSearch}>
