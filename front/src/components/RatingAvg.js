f83d3a547a437e5a4f9f3f5896f739441fef3f83
zzannorita <tpals0518@naver.com>
2024-12-12 00:24:46 +0900

    Fix: 변수에 따라 별점 칠하는 로직 변경

--- front/src/components/RatingAvg.js
+++ front/src/components/RatingAvg.js
@@ -1,25 +1,22 @@
 import React from "react";
 import RatingAvg from "../styles/ratingAvg.module.css";
-export default function RatingStarts() {
+
+export default function RatingStars({ rating }) {
   return (
     <div className={RatingAvg.myShopStarBox}>
       <div className={RatingAvg.myShopStars}>
         <div className={RatingAvg.rating}>
-          <span className={RatingAvg.star} data-value="5">
-            &#9733;
-          </span>
-          <span className={RatingAvg.star} data-value="4">
-            &#9733;
-          </span>
-          <span className={RatingAvg.star} data-value="3">
-            &#9733;
-          </span>
-          <span className={RatingAvg.star} data-value="2">
-            &#9733;
-          </span>
-          <span className={RatingAvg.star} data-value="1">
-            &#9733;
-          </span>
+          {[5, 4, 3, 2, 1].map((value) => (
+            <span
+              key={value}
+              className={`${RatingAvg.star} ${
+                value <= rating ? RatingAvg.activeStar : ""
+              }`}
+              data-value={value}
+            >
+              &#9733;
+            </span>
+          ))}
         </div>
       </div>
     </div>
