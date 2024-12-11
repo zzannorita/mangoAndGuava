d941098e7542b3a580b5add1644ccafc64e02008
zzannorita <tpals0518@naver.com>
2024-12-11 18:48:35 +0900

    Feat: 별점 등록 컴포넌트 생성

--- front/src/components/RatingStars.js
+++ front/src/components/RatingStars.js
@@ -1,27 +1,25 @@
 import React from "react";
-import RatingStars from "../styles/ratingStars.module.css";
-export default function RatingStarts() {
+import styles from "../styles/ratingStars.module.css"; // 별점 스타일 모듈 임포트
+
+function RatingStars({ rating, setRating }) {
+  // 별 클릭 시 호출되는 함수
+  const handleStarClick = (index) => {
+    setRating(index + 1); // 클릭한 별의 인덱스를 기준으로 별점 설정 (1~5)
+  };
+
   return (
-    <div className={RatingStars.myShopStarBox}>
-      <div className={RatingStars.myShopStars}>
-        <div className={RatingStars.rating}>
-          <span className={RatingStars.star} data-value="5">
-            &#9733;
-          </span>
-          <span className={RatingStars.star} data-value="4">
-            &#9733;
-          </span>
-          <span className={RatingStars.star} data-value="3">
-            &#9733;
-          </span>
-          <span className={RatingStars.star} data-value="2">
-            &#9733;
-          </span>
-          <span className={RatingStars.star} data-value="1">
-            &#9733;
-          </span>
-        </div>
-      </div>
+    <div className={styles.ratingStars}>
+      {Array.from({ length: 5 }, (_, index) => (
+        <span
+          key={index}
+          className={index < rating ? styles.filledStar : styles.emptyStar}
+          onClick={() => handleStarClick(index)}
+        >
+          ★
+        </span>
+      ))}
     </div>
   );
 }
+
+export default RatingStars;
