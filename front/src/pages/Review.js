e10df98e79c1a09a23161b5b00a86244377d7040
zzannorita <tpals0518@naver.com>
2024-12-20 02:49:26 +0900

    Feat: 후기페이지 프로필 연동 및 정렬

--- front/src/pages/Review.js
+++ front/src/pages/Review.js
@@ -8,6 +8,7 @@ import getRelativeTime from "../utils/getRelativeTime";
 export default function Review() {
   const [comments, setComments] = useState([]);
   const [commentCount, setCommentCount] = useState("");
+  const [sortType, setSortType] = useState("newest");
   useEffect(() => {
     axiosInstance
       .get("/myShop")
@@ -25,17 +26,35 @@ export default function Review() {
             userNickname: user?.nickname || user?.userId,
             commentContent: comment.comment,
             commentDate: getRelativeTime(comment.commentDate),
+            originalDate: comment.commentDate, // 정렬위해 원본 날짜
             commentAvg: comment.avg,
+            profileImage: user.profileImage,
           };
         });
-
-        setComments(arrangeComments);
-        console.log("arrangeComments:", arrangeComments);
+        // 정렬 처리
+        const sortedComments = sortComments(arrangeComments);
+        setComments(sortedComments);
       })
       .catch((error) => {
         console.error("데이터 가져오기 실패", error);
       });
-  }, []);
+  }, [sortType]);
+
+  // 정렬 함수
+  const sortComments = (comments) => {
+    switch (sortType) {
+      case "newest":
+        return comments.sort(
+          (a, b) => new Date(b.originalDate) - new Date(a.originalDate)
+        ); // 최신순
+      case "highest":
+        return comments.sort((a, b) => b.commentAvg - a.commentAvg); // 별점 높은 순
+      case "lowest":
+        return comments.sort((a, b) => a.commentAvg - b.commentAvg); // 별점 낮은 순
+      default:
+        return comments;
+    }
+  };
 
   return (
     <div className={ReviewStyle.myProductsBox}>
@@ -48,9 +67,19 @@ export default function Review() {
             <RatingAvg rating={commentCount} />
           </div>
           <div className={shopStyle.mainTopRightBox}>
-            <div className={shopStyle.filterTextBox}>별점높은순</div>
+            <div
+              className={shopStyle.filterTextBox}
+              onClick={() => setSortType("highest")}
+            >
+              별점높은순
+            </div>
             <span>|</span>
-            <div className={shopStyle.filterTextBox}>별점낮은순</div>
+            <div
+              className={shopStyle.filterTextBox}
+              onClick={() => setSortType("lowest")}
+            >
+              별점낮은순
+            </div>
           </div>
         </div>
         {comments && comments.length > 0 ? (
@@ -59,7 +88,11 @@ export default function Review() {
               <div className={ReviewStyle.reviewList}>
                 <img
                   className={ReviewStyle.reviewImg}
-                  src={exImg}
+                  src={
+                    comment.profileImage
+                      ? `http://localhost:3001/profileImage/${comment.profileImage}`
+                      : exImg
+                  }
                   alt="exImg"
                 ></img>
                 <div className={ReviewStyle.reviewTextBox}>
