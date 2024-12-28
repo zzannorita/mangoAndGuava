e02e659d58f78d7d7c3f90785d1ff7b9ab3dff1e
zzannorita <tpals0518@naver.com>
2024-12-28 17:34:51 +0900

    Feat: 최근본상품 연동 예시 코드

--- front/src/components/MainLayout.js
+++ front/src/components/MainLayout.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Category from "./Category";
 import RealTime from "./RealTime";
 import MainLayoutStyle from "../styles/mainLayout.module.css";
 import exImg from "../image/exImg.png";
 import eyeImg from "../image/eye.png";
+import axiosInstance from "../axios";
 
 export default function MainLayout() {
+  useEffect(() => {
+    axiosInstance.get("/recent-view").then((response) => {
+      console.log("최근본상품", response);
+    });
+  }, []);
   return (
     <div className="container">
       <div className={MainLayoutStyle.container}>
