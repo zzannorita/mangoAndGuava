f4943e134eb80c217c58adcdd6db531605b7b431
zzannorita <tpals0518@naver.com>
2025-01-08 23:43:33 +0900

    Fix: 정렬 유틸 명시적으로 내보내기

--- front/src/utils/sortUtils.js
+++ front/src/utils/sortUtils.js
@@ -1,6 +1,4 @@
-// utils/sortUtils.js
-
-// 댓글 정렬 함수
+//별점 정렬렬
 export const sortComments = (comments, sortType) => {
   switch (sortType) {
     case "newest":
@@ -16,24 +14,19 @@ export const sortComments = (comments, sortType) => {
   }
 };
 
-// 상품 정렬 함수 (default로 export)
-const sortProducts = (products, sortType) => {
+//나머지 정렬
+export const sortProducts = (products, sortType) => {
   switch (sortType) {
     case "newest":
-      // 최신순 (상품 등록일 기준으로 정렬)
       return products.sort(
         (a, b) =>
           new Date(b.productCreatedDate) - new Date(a.productCreatedDate)
       );
     case "lowPrice":
-      // 저가순 (상품 가격 기준으로 정렬)
       return products.sort((a, b) => a.productPrice - b.productPrice);
     case "highPrice":
-      // 고가순 (상품 가격 기준으로 정렬)
       return products.sort((a, b) => b.productPrice - a.productPrice);
     default:
       return products;
   }
 };
-
-export default sortProducts; // default로 export
