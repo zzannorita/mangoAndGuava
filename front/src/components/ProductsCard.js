86d3986afb9a73d97890ef62465725f40666a9c9
zzannorita <tpals0518@naver.com>
2025-01-14 00:48:43 +0900

    Fix: 상품카드 이미지 받는 형식에 배열 추가

--- front/src/components/ProductsCard.js
+++ front/src/components/ProductsCard.js
@@ -79,7 +79,13 @@ const ProductsCard = ({ product, userId, type }) => {
       className={type === "home" ? HomeStyle.product : productStyle.productCard}
     >
       <img
-        src={product.images.length > 0 ? product.images[0].productImage : ""}
+        src={
+          product.images.length > 0
+            ? typeof product.images[0] === "object"
+              ? product.images[0].productImage
+              : product.images[0]
+            : ""
+        }
         alt={product.productName}
         className={
           type === "home"
