46fea08ad032bc694f6821b274c881c3d037be64
kyuchory <sls789456@naver.com>
2025-01-18 22:33:00 +0900

    Feat: 웹소켓 contextAPI 추가

--- front/src/App.js
+++ front/src/App.js
@@ -17,39 +17,42 @@ import ShopForBuyer from "./pages/ShopForBuyer";
 import Update from "./pages/Update";
 import Review from "./pages/Review";
 import OthersReview from "./pages/OthersReview";
+import { WebSocketProvider } from "./contexts/WebSocketContext";
 
 function App() {
   return (
-    <Router>
-      <div className="header">
-        <Header />
-      </div>
-      <hr className="separator" />
-      <div className="mainLayout">
-        <MainLayout />
-      </div>
-      <div className="mainContent">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sell" element={<Regist />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/products" element={<Search />} />
-          {/* <Route path="/products?item={search-data}" element={<Search />} /> */}
-          <Route path="/chat" element={<Chat />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/myShop" element={<Shop />} />
-          <Route path="/yourShop" element={<ShopForBuyer />} />
-          <Route path="/detail" element={<Detail />} />
-          <Route path="/update" element={<Update />} />
-          <Route path="/newMember" element={<NewMember />} />
-          <Route path="/authCallBack" element={<AuthCallback />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/othersReview" element={<OthersReview />} />
-          <Route path="/carousel" element={<Carousel />} />
-          {/* <Route path="/detail?itemId={productId}" element={<Detail />} /> */}
-        </Routes>
-      </div>
-    </Router>
+    <WebSocketProvider>
+      <Router>
+        <div className="header">
+          <Header />
+        </div>
+        <hr className="separator" />
+        <div className="mainLayout">
+          <MainLayout />
+        </div>
+        <div className="mainContent">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/sell" element={<Regist />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/products" element={<Search />} />
+            {/* <Route path="/products?item={search-data}" element={<Search />} /> */}
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/myShop" element={<Shop />} />
+            <Route path="/yourShop" element={<ShopForBuyer />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/update" element={<Update />} />
+            <Route path="/newMember" element={<NewMember />} />
+            <Route path="/authCallBack" element={<AuthCallback />} />
+            <Route path="/review" element={<Review />} />
+            <Route path="/othersReview" element={<OthersReview />} />
+            <Route path="/carousel" element={<Carousel />} />
+            {/* <Route path="/detail?itemId={productId}" element={<Detail />} /> */}
+          </Routes>
+        </div>
+      </Router>
+    </WebSocketProvider>
   );
 }
 
