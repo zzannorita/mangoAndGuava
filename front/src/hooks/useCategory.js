932298a95429314c84f6899ba5426578a6311d37
zzannorita <tpals0518@naver.com>
2025-02-26 16:41:29 +0900

    Fix: 카테고리 최종 수정

--- front/src/hooks/useCategory.js
+++ front/src/hooks/useCategory.js
@@ -105,17 +105,17 @@ const categories = [
         link: "/products?category=200220",
         subSubCategories: [
           {
-            code: "200220221",
+            code: "200220211",
             name: "양문형",
             link: "/products?category=200220211",
           },
           {
-            code: "200220222",
+            code: "200220212",
             name: "미니 냉장고",
             link: "/products?category=200220212",
           },
           {
-            code: "200220223",
+            code: "200220213",
             name: "와인 냉장고",
             link: "/products?category=200220213",
           },
@@ -127,17 +127,17 @@ const categories = [
         link: "/products?category=200230",
         subSubCategories: [
           {
-            code: "200230231",
+            code: "200230211",
             name: "드럼 세탁기",
             link: "/products?category=200230211",
           },
           {
-            code: "200230232",
+            code: "200230212",
             name: "통돌이 세탁기",
             link: "/products?category=200230212",
           },
           {
-            code: "200230233",
+            code: "200230213",
             name: "미니 세탁기",
             link: "/products?category=200230213",
           },
@@ -178,17 +178,17 @@ const categories = [
         link: "/products?category=300320",
         subSubCategories: [
           {
-            code: "300320321",
+            code: "300320311",
             name: "일반 팬",
             link: "/products?category=300320311",
           },
           {
-            code: "300320322",
+            code: "300320312",
             name: "논스틱 팬",
             link: "/products?category=300320312",
           },
           {
-            code: "300320323",
+            code: "300320313",
             name: "그릴 팬",
             link: "/products?category=300320313",
           },
@@ -200,17 +200,17 @@ const categories = [
         link: "/products?category=300330",
         subSubCategories: [
           {
-            code: "300330331",
+            code: "300330311",
             name: "접시",
             link: "/products?category=300330311",
           },
           {
-            code: "300330332",
+            code: "300330312",
             name: "컵",
             link: "/products?category=300330312",
           },
           {
-            code: "300330333",
+            code: "300330313",
             name: "수저",
             link: "/products?category=300330313",
           },
